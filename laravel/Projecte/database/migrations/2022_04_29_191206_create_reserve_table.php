6ad1623b31c87fba12b71a5989e63bfecddd02cc
jorocaa <joroca@fp.insjoaquimmir.cat>
2022-04-29 21:15:04 +0200

    create table Reserve (se encarga de la accion de reservar) + create model Reserve

--- laravel/Projecte/database/migrations/2022_04_29_191206_create_reserve_table.php
+++ laravel/Projecte/database/migrations/2022_04_29_191206_create_reserve_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateReservationTable extends Migration
+class CreateReserveTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,8 @@ class CreateReservationTable extends Migration
      */
     public function up()
     {
-        Schema::create('reservation', function (Blueprint $table) {
+        Schema::create('reserve', function (Blueprint $table) {
             $table->id();
-            $table->string('reservationlink');
-            $table->string('namecompany');
-            $table->foreignId('idclientcreateblog')->references('id')->on('users');
-            $table->foreignId('idmoderator')->references('id')->on('users')->constrained();
             $table->timestamps();
         });
     }
@@ -30,6 +26,6 @@ class CreateReservationTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('reservation');
+        Schema::dropIfExists('reserve');
     }
 }
