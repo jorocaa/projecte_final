b96a4651ddec13d03d6f000fa1ab048212dda23f
jorocaa <joroca@fp.insjoaquimmir.cat>
2022-05-05 21:16:47 +0200

    22-5-5 MailController added

--- laravel/Projecte/app/Http/Controllers/MailController.php
+++ laravel/Projecte/app/Http/Controllers/MailController.php
@@ -2,19 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\MailReserve;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class MailController extends Controller
 {
-    public function __invoke(Request $request)
+    public function test(Request $request)
     {
         try {
-            $mail = new TestMail([
+            $mail = new MailReserve([
                 'name' => 'Anonymous',
                 'body' => 'Testing mail',
                 'url'  => '/'
             ]);
-            \Mail::to('example@example.com')->send($mail);
+            Mail::to('joroca@fp.insjoaquimmir.cat')->send($mail);
             echo '<h1>Mail send successfully</h1>';
         } catch (\Exception $e) {
             echo '<pre>Error - ' . $e .'</pre>';
