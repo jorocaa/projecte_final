259409de6e2b2ff23aa8801c1ad18b3df1ec73f2
jorocaa <joroca@fp.insjoaquimmir.cat>
2022-05-13 17:53:50 +0200

    22-5-13 vista 404 error

---
+++ laravel/Projecte/resources/views/errors/404.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    @include('head')
+</head>
+<body class="antialiased">
+    <h1>INTRODUCE LO Q QRAS 404 ERROR</h1>
+</body>
+</html>
