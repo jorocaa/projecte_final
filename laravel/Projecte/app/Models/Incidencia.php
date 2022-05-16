e8c807198cfc3b89bd0f6e8088dc065578375613
jorocaa <joroca@fp.insjoaquimmir.cat>
2022-05-16 15:55:01 +0200

    22-5-16 model

---
+++ laravel/Projecte/app/Models/Incidencia.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Incidencia extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'idclient',
+        'idmoderator',
+        'title',
+        'type',
+        'description',
+    ];
+}
